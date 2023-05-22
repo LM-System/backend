6e6d384a142c09619fe864f91d6089cd9c58e298
MohammedAbuEssa <mabuissa5@gmail.com>
2023-05-22 11:11:38 +0000

    AbuEssa

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-"use strict"
+"use strict";
 require("dotenv").config();
-const express=require("express");
-const cors=require("cors");
-const pg=require("pg");
-const app= express();
-const client =new pg.Client(/*postgressSql URL*/);
+const express = require("express");
+const cors = require("cors");
+const pg = require("pg");
+const app = express();
+const client = new pg.Client(/*postgressSql URL*/);
 app.use(cors());
 app.use(express.json());
 
+app.post("/addCourse", handeleAddCourse);
 
-app.listen(4000,()=>{
-    console.log(`server is running is port ${4000}`)
-})
+function handeleAddCourse(req, res) {
+  const course = res.body;
+  const sql = `INSERT into course (title,descreption,capacity,role,u_id) values ('${course.title}','${course.descreption}','${course.role}','${course.u_id}');`;
+  client.query(sql).then(() => {
+    res.send("Course added successfully");
+  });
+}
+
+app.listen(4000, () => {
+  console.log(`server is running is port ${4000}`);
+});
