9fda1a224be2ee1c281420769b3054c4bae0e3d2
Malek Al-Sheekh Hasan <malek.alsheekh01@gmail.com>
2023-05-23 05:17:38 +0000

    updatestudent endpoint to update the informatin of the student

--- server.js
+++ server.js
@@ -33,6 +33,21 @@ app.post('/addstudent',(req,res)=>{
     });
 });
 
+app.put('/updatestudent/:id',(req,res)=>{
+    const {id}=req.params;
+    const {email,password,fname,lname,status}=req.body;
+    const role="student";
+    const sql=`update users set
+    email=$1,password=$2,fname=$3,lname=$4,role=$5,status=$6
+    where id=${id} returning *;`;
+    client.query(sql,[email,password,fname,lname,role,status]).then((data)=>{
+        console.log(data);
+        res.status(200).send(data.rows);
+    }).catch((e)=>{
+        console.log(e);
+    });
+});
+
 client.connect().then(() => {
     app.listen(port, () => {
     console.log(`server is running is port ${port}`);
