9e6b40d506ff759a0c8aa036a5ceb19f3dd1272c
MohammedAbuEssa <mabuissa5@gmail.com>
2023-05-23 07:13:42 +0000

    Added add course port and get course port

--- server.js
+++ server.js
@@ -9,9 +9,31 @@ const client = new pg.Client(process.env.DATABASE_URL);
 app.use(cors());
 app.use(express.json());
 
-app.post("/addCourse", handeleAddCourse);
+app.get("/", function (req, res) {
+  res.send("Connected");
+});
+
+app.post("/addcourse", handeleAddCourse); //Add Course end point
+app.get("/getcourse", handleGetCourse);
+
+function handleGetCourse(req, res) {
+  const sql = "select * from course;";
+  client.query(sql).then((data) => {
+    let dataFromDB = data.rows;
+    res.send(dataFromDB);
+  });
+}
+
+app.get("/signin", (req, res) => {
+  const { email, password } = req.body;
+  const sql = `select * from users where email=$1 AND password=$2`;
+  client.query(sql, [email, password]).then((data) => {
+    res.status(200).send(data.rows);
+  });
+});
 
 function handeleAddCourse(req, res) {
+  // Add course function
   const course = res.body;
   const sql = `INSERT into course (title,descreption,capacity,role,u_id) values ('${course.title}','${course.descreption}','${course.role}','${course.u_id}');`;
   client.query(sql).then(() => {
@@ -19,10 +41,8 @@ function handeleAddCourse(req, res) {
   });
 }
 
-app.listen(4000, () => {
-  console.log(`server is running is port ${4000}`);
-});
-
-app.listen(4000, () => {
-  console.log(`server is running is port ${4000}`);
+client.connect().then(() => {
+  app.listen(port, () => {
+    console.log(`server is running is port ${port}`);
+  });
 });
