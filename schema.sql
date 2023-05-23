1de783df7c9bf59db509e3fde76b413d0e4d657c
Malek Al-Sheekh Hasan <malek.alsheekh01@gmail.com>
2023-05-23 08:24:33 +0000

    create deletestudent endpoint to delete the student from tabel users

--- schema.sql
+++ schema.sql
@@ -8,9 +8,7 @@ CREATE TABLE IF NOT EXISTS users(
     role varchar(255),
     status varchar(255)
 );
-INSERT INTO users(email,password,fname,lname,role,status) VALUES ('mhd@mhd.com','1234','mhd','mhd','admin','on');
-INSERT INTO users(email,password,fname,lname,role,status) VALUES ('ahd@mhdcom','1234','ahmad','shihab','teacher','on');
-INSERT INTO users(email,password,fname,lname,role,status) VALUES ('musa@musa.com','1234','musa','bilal','student','on');
+
 CREATE TABLE IF NOT EXISTS course(
     id SERIAL PRIMARY KEY,
     title varchar(255),
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", function (req, res) {
-  res.send("Connected");
+    res.send("Connected");
 });
 
 app.post("/addcourse", handeleAddCourse); //Add Course end point
@@ -18,69 +18,85 @@ app.get("/getcourse", handleGetCourse);
 
 function handleGetCourse(req, res) {
   const sql = "select * from course;";
-  client.query(sql).then((data) => {
+    client.query(sql).then((data) => {
     let dataFromDB = data.rows;
     res.send(dataFromDB);
-  });
+    });
 }
 
 function handeleAddCourse(req, res) {
   // Add course function
-  const course = res.body;
-  const sql = `INSERT into course (title,descreption,capacity,role,u_id) values ('${course.title}','${course.descreption}','${course.role}','${course.u_id}');`;
-  client.query(sql).then(() => {
+    const course = res.body;
+    const sql = `INSERT into course (title,descreption,capacity,role,u_id) values ('${course.title}','${course.descreption}','${course.role}','${course.u_id}');`;
+    client.query(sql).then(() => {
     res.send("Course added successfully");
-  });
+    });
 }
 app.post("/signin", (req, res) => {
-  const { email, password } = req.body;
-  const sql = `select * from users where email=$1 AND password=$2;`;
-  client
+    const { email, password } = req.body;
+    const sql = `select * from users where email=$1 AND password=$2;`;
+    client
     .query(sql, [email, password])
     .then((data) => {
-      res.status(200).send(data.rows);
+        res.status(200).send(data.rows);
     })
     .catch((e) => {
-      console.log(e);
+        console.log(e);
     });
 });
 
 app.post("/addstudent", (req, res) => {
-  const { email, password, fname, lname } = req.body;
-  const status = "off";
-  const role = "student";
-  const sql = `INSERT INTO users(email,password,fname,lname,role,status) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;`;
-  client
+    const { email, password, fname, lname } = req.body;
+    const status = "off";
+    const role = "student";
+    const sql = `INSERT INTO users(email,password,fname,lname,role,status) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *;`;
+    client
     .query(sql, [email, password, fname, lname, role, status])
     .then((data) => {
-      console.log(data);
-      res.status(200).send(data.rows);
+        console.log(data);
+        res.status(200).send(data.rows);
     })
     .catch((e) => {
-      console.log(e);
+        console.log(e);
     });
 });
 
 app.put("/updatestudent/:id", (req, res) => {
-  const { id } = req.params;
-  const { email, password, fname, lname, status } = req.body;
-  const role = "student";
-  const sql = `update users set
+    const { id } = req.params;
+    const { email, password, fname, lname, status } = req.body;
+    const role = "student";
+    const sql = `update users set
     email=$1,password=$2,fname=$3,lname=$4,role=$5,status=$6
     where id=${id} returning *;`;
-  client
+    client
     .query(sql, [email, password, fname, lname, role, status])
     .then((data) => {
-      console.log(data);
-      res.status(200).send(data.rows);
+        console.log(data);
+        res.status(200).send(data.rows);
     })
     .catch((e) => {
-      console.log(e);
+        console.log(e);
+    });
+});
+
+app.delete("/deletestudent/:id", (req, res) => {
+    const { id } = req.params;
+    const sql = `DELETE from users where id=${id};`;
+    client
+    .query(sql)
+    .then((data) => {
+        console.log(data);
+        res.status(200).send(data.rows);
+
+    })
+    .catch((e) => {
+        console.log(e);
     });
 });
 
 client.connect().then(() => {
-  app.listen(port, () => {
+    app.listen(port, () => {
     console.log(`server is running is port ${port}`);
-  });
+    });
 });
+// "`[{title:`javascript Course`,startDate:`22-5-2023`,endDate:`22-5-2023`},{title:`DotNet Course`,startDate:`22-5-2023`,endDate:`22-5-2023`}]`"
